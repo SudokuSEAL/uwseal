d5f011940002e7b7e71290a39896037b6a8e079e
markopmilo <marko.p.milovanovich@gmail.com>
2022-02-27 14:10:41 -0800

    deleting
    deleted unnecessary files

--- people_files/nav.js
+++
@@ -1,9 +0,0 @@
-document.write('<table cellpadding=0 cellspacing=0>');
-document.write('<tr><td><a href="index.html">Home</a>');
-document.write('<tr><td><a href="about.html">About Us</a>');
-document.write('<tr><td><a href="research.html">Research</a>');
-document.write('<tr><td><a href="people.html">People</a>');
-document.write('<tr><td><a href="assessment.html">Your Assessment</a>');
-document.write('<tr><td><a href="contact.html">Contact Us</a>');
-document.write('<tr><td><a href="links.html">Other Websites</a>');
-document.write('</table>');
